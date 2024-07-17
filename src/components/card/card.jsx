e27dff6c378e3b1ca884e21c9d74c71d89f30fd6
nurmuhammad <nurmuhammadeshonov97@gmail.com>
2024-07-17 11:25:44 +0500

    [added] navbar

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,22 +3,24 @@ import "./card.css";
 function Card(props) {
   const { data } = props;
   return (
-    <div className="card">
-      <img className="image" src={data.image} alt="" />
+    <>
+      <div className="cardd">
+        <img className="image" src={data.image} alt="" />
 
-      <div className="wrapper">
-        <div>
-          <h2 className="firstN">{data.firstName}</h2>
-          <p className="lastN">{data.lastName}</p>
-          <p className="cars">{data.cars[1]}</p>
+        <div className="wrapper">
+          <div>
+            <h2 className="firstN">{data.firstName}</h2>
+            <p className="lastN">{data.lastName}</p>
+            <p className="cars">{data.cars[1]}</p>
 
-          <p className="phone">{data.phone}</p>
-        </div>
+            <p className="phone">{data.phone}</p>
       <p className="email">{data.email}</p>
-       
-      </div>
 
-    </div>
+          </div>
+        </div>
+      </div>
+      <p/>
+    </>
   );
 }
 
